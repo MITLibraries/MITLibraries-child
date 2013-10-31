6ca39f0e88b93d3d5eae761e21b8dbc6e452a5d7
Zach Green <zgreen@mit.edu>
2013-10-31 10:54:07 -0400

    Fixed switch_to_blog
    Removed an unessecary switch to the main blog.

--- inc/postHead.php
+++ inc/postHead.php
@@ -1,38 +1,5 @@
-<?php
+<h1 class="siteTitle"><?php bloginfo(); ?></h1>
 
-	global $blog_id;
-	$current_blog_id = $blog_id;
-
-	//switch to the main blog which will have an id of 1
-	switch_to_blog(1);
-
-	if($current_blog_id = 22) {
-
-		$locationID = 452;
-
-		$hasHours = hasHours($locationId, date("Y-m-d"));
-		$hoursToday = getHoursToday($locationId);
-		switch_to_blog($current_blog_id);
-
-	}
-
-	else {
-		echo "<h1>this is not the main site, or the function is not working</h1>";
-	}
-
-?>
-
-<div class="siteTitle">
-
-	<h1><?php bloginfo(); ?></h1>
-
-	<div class="todayHours">
-		<span>Today's Hours:</span>
-		<span><strong>9am-5pm (by appointment only)</strong></span>
-	</div>
-
-</div>
-	
 <?php if (has_post_thumbnail()): ?>
 
 	<div class="headerImage">
@@ -41,16 +8,9 @@
 
 	<?php else: ?>
 
-	<?php
-		$defaultHeader = header_image();
-		if ($defaultHeader != "") {
-	?>
-	
-		<div class="headerImage">
-			<img src="<?php header_image(); ?>" alt="" />
-		</div>
-
-	<?php } ?>	
+	<div class="headerImage">
+		<img src="<?php header_image(); ?>" alt="" />
+	</div>	
 	
 <?php endif; ?>		
 			
