1bb1a96ebea76cd097670852383cb3b9185092b5
Zach Green <zgreen@mit.edu>
2013-11-20 11:09:30 -0500

    Added location info & homepage columns
    w/ CSS updates

--- inc/content-page.php
+++ inc/content-page.php
@@ -15,10 +15,33 @@ global $isRoot;
 			<?php $title = get_the_title(); if ($title != ""): ?>
 				<h2><?php the_title(); ?></h2>
 			<?php endif; ?>
-			
+
 			<?php the_content(); ?>
-			
+
+			<?php
+				$blog_id = get_current_blog_id();
+
+				if (is_page('home') && $blog_id == 22):
+					$col1 = get_field('column_1');
+					$col2 = get_field('column_2');
+				endif;
+				
+				if ($col1 != '' && $col2 != ''):
+			?>
+
+					<div class="flexContainer">
+						<div class="flexItem">
+							<?php echo $col1; ?>
+						</div>
+						<div class="flexItem">
+							<?php echo $col2; ?>
+						</div>
+					</div>
+
+			<?php endif; ?>
+
 		</div>
+		
 		<footer class="entry-meta">
 			<?php edit_post_link( __( 'Edit', 'twentytwelve' ), '<span class="edit-link">', '</span>' ); ?>
 		</footer><!-- .entry-meta -->
--- inc/locationHours.php
+++ inc/locationHours.php
@@ -12,17 +12,29 @@
 
 	$hasHours = hasHours($locationId, date("Y-m-d"));
 	$hoursToday = getHoursToday($locationId);
-
-	$divOpen = '<div class="todayHours">';
-	$spanOpen = '<span>';
-	$spanClose = '</span>';
-	$todaysHours = "<span>Today's Hours:</span>";
-	$timeOpen = '<span class="time">';
-	$timeClose = '<span>';
-	$divClose = '</div>';
-
-	echo $divOpen.$spanOpen.$todaysHours.$spanClose.$spanOpen.$hoursToday.$spanClose.$spanOpen.'<a href="/hours">See all hours</a>'.$spanClose.$divClose;
-
-	switch_to_blog($current_blog_id);
-	
 ?>
+
+	<div class="todayHours">
+		<span>Today's Hours:</span>
+		<span><?php echo $hoursToday; ?></span>
+		<span><a href="/hours">See all hours</a></span>
+	</div>
+
+<?php
+	
+	$docsServices = get_post(1028);
+	$docsPhone = get_post_meta(1028, 'phone', true);
+	$docsBuilding = get_post_meta(1028, 'building', true);
+	$docsSlug = $docsServices->post_name;
+
+	$mapPage = "/locations/#!";
+	$slug = $post->post_name;
+
+?>
+
+	<div class="locationInfo">
+		<span><?php echo $docsPhone; ?></span>
+		<span>show on map: <a href="<?php echo $mapPage.$docsSlug; ?>"><?php echo $docsBuilding ?></a></span>
+	</div>
+	
+<?php switch_to_blog($current_blog_id); ?>
