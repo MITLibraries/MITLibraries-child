683a32013f96a4f57a3c0af812eec4381f442edf
Zach Green <zgreen@mit.edu>
2014-05-19 10:19:59 -0400

    Use class "nav-secondary" on child theme nav

--- inc/nav-child.php
+++ inc/nav-child.php
@@ -4,8 +4,16 @@
 	$noChildNav = array("MIT Libraries News", "Document Services");
 	if (!in_array($siteName, $noChildNav)) {
 ?>
-	<nav id="child-navigation" class="main-navigation blogNav childNav" role="navigation">
+	<nav id="child-navigation" class="main-navigation blogNav nav-secondary" role="navigation">
 		<h3 class="menu-toggle"><?php _e( 'Child Menu', 'twentytwelve' ); ?></h3>
-		<?php wp_nav_menu( array( 'theme_location' => 'child', 'menu_class' => 'nav-menu' ) ); ?>
+		<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'child',
+					'container' => 'nav',
+					'container_class' => 'nav-main',
+				)
+			);
+		?>
 	</nav><!-- #site-navigation -->
 <?php } ?>
