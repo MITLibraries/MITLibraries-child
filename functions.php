e5184be25ed657ecad69b426ebe9403b492358be
pbruk <phillip.bruk@gmail.com>
2015-07-22 14:35:38 -0400

    add scripts/styles for subnav

--- functions.php
+++ functions.php
@@ -6,6 +6,20 @@ function remove_child_theme_support() {
   remove_theme_support('custom-background');
 }
 
+	function enqueue_my_scripts() {
+	wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', array('jquery'), '1.9.1', true); // we need the jquery library
+	wp_enqueue_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array('jquery'), true); // all the bootstrap javascript goodness
+	}
+	add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
+	function enqueue_my_styles() {
+	wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
+	wp_enqueue_style( 'roger', get_template_directory_uri() . '/style.css');
+	}
+	add_action('wp_enqueue_scripts', 'enqueue_my_styles');
+	
+
+
+
 function betterChildBreadcrumbs() {
 
   global $post;
