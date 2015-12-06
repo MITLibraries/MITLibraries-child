4a5aef8bdf0399d9b0afdf863287f70b48901e3b
pbruk <phillip.bruk@gmail.com>
2015-12-06 15:51:50 -0500

    Add post pagination

--- functions.php
+++ functions.php
@@ -77,11 +77,25 @@ if(!function_exists('register_child_nav')) {
 
 
 
+/**
+ * Load all custom post_types if home page or search results.
+ */
+function search_filter($query) {
+  if ( !is_admin() && $query->is_main_query() ) {
+     if ($query->is_home() || $query->is_search() ) {
+      $query->set('post_type', array( 'maihaugen', 'rotch', 'online' ) );
+    }
+  }
+}
+add_action('pre_get_posts','search_filter');
+
+
 
 	
 if (function_exists('add_theme_support')) { add_theme_support('post-thumbnails'); }
 
 add_image_size('headerImage', 480, 200, true);
+add_image_size('exhibitImage', 150, 150, true);
 
 if (!function_exists('my_mce_buttons_2')):
   function my_mce_buttons_2($buttons) { 
@@ -96,5 +110,86 @@ if (!function_exists('my_mce_buttons_2')):
   add_filter('mce_buttons_2', 'my_mce_buttons_2');
 endif;
 
+function add_custom_types_to_tax( $query ) {
+if( is_category() || is_tag() && empty( $query->query_vars['suppress_filters'] ) ) {
+
+// Get all your post types
+$post_types = get_post_types();
+
+$query->set( 'post_type', $post_types );
+return $query;
+}
+}
+add_filter( 'pre_get_posts', 'add_custom_types_to_tax' );
+
+// pagination for posts
+function child_numeric_posts_nav() {
+
+	if( is_singular() )
+		return;
+
+	global $wp_query;
+
+	/** Stop execution if there's only 1 page */
+	if( $wp_query->max_num_pages <= 1 )
+		return;
+
+	$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
+	$max   = intval( $wp_query->max_num_pages );
+
+	/**	Add current page to the array */
+	if ( $paged >= 1 )
+		$links[] = $paged;
+
+	/**	Add the pages around the current page to the array */
+	if ( $paged >= 3 ) {
+		$links[] = $paged - 1;
+		$links[] = $paged - 2;
+	}
+
+	if ( ( $paged + 2 ) <= $max ) {
+		$links[] = $paged + 2;
+		$links[] = $paged + 1;
+	}
+
+	echo '<div class="post-navigation"><ul>' . "\n";
+
+	/**	Previous Post Link */
+	if ( get_previous_posts_link() )
+		printf( '<li>%s</li>' . "\n", get_previous_posts_link() );
+
+	/**	Link to first page, plus ellipses if necessary */
+	if ( ! in_array( 1, $links ) ) {
+		$class = 1 == $paged ? ' class="active"' : '';
+
+		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
+
+		if ( ! in_array( 2, $links ) )
+			echo '<li>…</li>';
+	}
+
+	/**	Link to current page, plus 2 pages in either direction if necessary */
+	sort( $links );
+	foreach ( (array) $links as $link ) {
+		$class = $paged == $link ? ' class="active"' : '';
+		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $link ) ), $link );
+	}
+
+	/**	Link to last page, plus ellipses if necessary */
+	if ( ! in_array( $max, $links ) ) {
+		if ( ! in_array( $max - 1, $links ) )
+			echo '<li>…</li>' . "\n";
+
+		$class = $paged == $max ? ' class="active"' : '';
+		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
+	}
+
+	/**	Next Post Link */
+	if ( get_next_posts_link() )
+		printf( '<li>%s</li>' . "\n", get_next_posts_link() );
+
+	echo '</ul></div>' . "\n";
+
+}
 
 
