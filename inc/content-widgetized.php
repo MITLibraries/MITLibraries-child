cfbe8179bbaa899a0d0093a785c4d99f2808ec1a
pbruk <phillip.bruk@gmail.com>
2016-02-10 15:48:44 -0500

    Add page-template and template-part to allow for widgetized sections below the content.

---
+++ inc/content-widgetized.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template used for displaying
+ * page content for Widgetized Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+global $isRoot;
+?>
+
+<?php
+
+	$pageRoot = getRoot($post);
+	$section = get_post($pageRoot);
+
+?>
+
+
+<div class="mainContent group content-main">
+	
+	<div class="entry-content">
+		<?php $title = get_the_title(); if ($title != ""): ?>
+			<?php if(!is_front_page()) { echo "<h1>".$title."</h1>"; } ?>
+		<?php endif; ?>
+
+		<?php the_content(); ?>
+		
+		<?php get_sidebar( 'front' ); ?>
+
+	</div>
+	
+		
+</div>
---
+++ page-widgetized.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ *
+  * Template Name: Widgetized Page
+
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+ 
+$pageRoot = getRoot($post);
+$section = get_post($pageRoot);
+$isRoot = $section->ID == $post->ID;
+
+
+get_header(); 
+
+get_template_part('inc/breadcrumbs', 'child'); ?>
+
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		
+
+		<div id="stage" class="inner group" role="main">
+
+			<?php get_template_part('inc/postHead'); ?>
+			
+			<?php if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+			
+				<div id="content" class="allContent hasSidebar group">
+					
+					
+
+				<?php get_template_part( 'inc/content', 'widgetized' ); ?>
+
+				<?php get_sidebar(); ?>
+
+			</div>
+			
+			<?php } else { ?> 
+			
+			<div id="content" class="allContent group">
+
+				<?php get_template_part( 'inc/content', 'widgetized' ); ?>
+
+			</div>
+			
+			<?php } ?>
+		
+		</div><!-- end div#stage -->
+		
+	<?php endwhile; // end of the loop. ?>
+
+
+<?php get_footer(); ?>
