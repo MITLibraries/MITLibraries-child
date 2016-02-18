439d5434ff33a6f48ff703eda1a6bcfc0e4ff5f2
pbruk <phillip.bruk@gmail.com>
2016-02-18 17:13:55 -0500

    Create new Front-Page templates to display sticky post; add styling for sticky post and other flex-box content.

---
+++ inc/content-front.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template used for displaying
+ * page content in page-front.php
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+global $isRoot;
+?>
+
+
+<div class="main-content content-main">
+	
+	<div class="entry-content">
+		
+				<?php
+				$sticky = get_option( 'sticky_posts' );
+				$the_query = new WP_Query( 'p=' . $sticky[0] );
+				while ($the_query->have_posts()) {
+					$the_query->the_post();
+				?>
+				
+				<div class="excerpt-post">
+					<img class="excerpt-post__fig"  src="<?php echo get_first_post_image(); ?>" width="200" >
+					<div class="excerpt-post__body">
+		                <h3><a href="<?php echo the_permalink();?>"><?php echo get_the_title();?></a></h3>
+		                <?php echo the_excerpt();?>
+					</div>
+				</div>
+				
+				<?php } wp_reset_postdata(); ?>
+
+		<?php the_content(); ?>
+		
+		</div>
+	
+		
+</div>
---
+++ page-front.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *
+ * Template Name: Front Page
+
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+
+	<?php if ( is_front_page()) { ?>
+
+	 <div class="betterBreadcrumbs hidden-phone" role="navigation" aria-label="breadcrumbs">
+      <span><a href="/">Libraries home</a></span>
+      <span><?php bloginfo(); ?></span>
+    </div>
+
+    <?php } else { ?>
+
+		<?php get_template_part('inc/breadcrumbs', 'child'); ?>
+
+			<?php } ?>
+
+		<div id="stage" class="inner" role="main">
+
+			<?php get_template_part('inc/postHead'); ?>
+
+
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php if ( is_active_sidebar( 'sidebar' ) ) { ?>
+
+				<div id="content" class="content has-sidebar">
+
+				<?php get_template_part( 'inc/content', 'front' ); ?>
+
+				<?php get_sidebar(); ?>
+
+			</div>
+
+			<?php } else { ?>
+
+			<div id="content" class="content">
+
+				<?php get_template_part( 'inc/content', 'front' ); ?>
+
+			</div>
+
+			<?php } ?>
+
+		</div><!-- end div#stage -->
+
+	<?php endwhile; // end of the loop. ?>
+
+<?php get_footer(); ?>
