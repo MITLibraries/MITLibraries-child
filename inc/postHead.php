0b596cd96d5a780ced06a69dd2ddc81d0cb30f95
pbruk <phillip.bruk@gmail.com>
2016-03-11 15:07:43 -0500

    Set header images to load as background images via CSS.

--- inc/postHead.php
+++ inc/postHead.php
@@ -2,36 +2,36 @@
 		<?php if(is_front_page()): ?>
 			<div class="child-header-tall">
 				<div class="page-header-home">
-					<h1 class="child-page-title"><?php bloginfo(); ?></h1>
-					<p class="child-tagline"><?php bloginfo('description'); ?></p>
-					<?php
-
-						global $blog_id;
-						$current_blog_id = $blog_id;
-						
-						$siteName = get_bloginfo('name');
-						
-						if($siteName == 'Document Services' && is_front_page()) {
-							switch_to_blog(1);
-							get_template_part('inc/locationInfo');
-							switch_to_blog($current_blog_id);
-						}
-
-					?>
-
-					<?php
-						
-						// If doc. services, switch out to main site to get location ids
-						if ($siteName == 'Document Services' && is_front_page()):
-							get_template_part('inc/locationHours');
-						endif;
-				
-					?>
+						<h1 class="child-page-title"><?php bloginfo(); ?></h1>
+						<p class="child-tagline"><?php bloginfo('description'); ?></p>
+						<?php
+	
+							global $blog_id;
+							$current_blog_id = $blog_id;
+							
+							$siteName = get_bloginfo('name');
+							
+							if($siteName == 'Document Services' && is_front_page()) {
+								switch_to_blog(1);
+								get_template_part('inc/locationInfo');
+								switch_to_blog($current_blog_id);
+							}
+	
+						?>
+	
+						<?php
+							
+							// If doc. services, switch out to main site to get location ids
+							if ($siteName == 'Document Services' && is_front_page()):
+								get_template_part('inc/locationHours');
+							endif;
+					
+						?>
 					
 				</div>
 					
 				<div class="header-bg-image-high">
-					<img class="test-image" src="<?php header_image(); ?>" alt="" />
+					<img class="header-image-high" style="background-image: url('<?php header_image(); ?>');" />
 				</div>
 				
 			</div>
@@ -43,7 +43,7 @@
 					<div class="child-page-title"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></div>
 				</div>
 				<div class="header-bg-image-short">
-					<?php the_post_thumbnail(); ?>
+					<img class="header-image-short" style="background-image: url('<?php the_post_thumbnail(); ?>');" />
 				</div>			
 			</div>
 			
