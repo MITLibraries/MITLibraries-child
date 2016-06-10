9024ebdcc67829286eb48988fc30b99ddca23fb7
Matt Bernhardt <mjbernha@mit.edu>
2016-06-10 09:13:12 -0400

    Automated cleaning of Generic.ControlStructures.InlineControlStructure

--- functions.php
+++ functions.php
@@ -171,21 +171,21 @@ add_filter( 'pre_get_posts', 'add_custom_types_to_tax' );
 // pagination for posts
 function child_numeric_posts_nav() {
 
-	if ( is_singular() )
-		return;
+	if ( is_singular() ) {
+		return; }
 
 	global $wp_query;
 
 	/** Stop execution if there's only 1 page */
-	if ( $wp_query->max_num_pages <= 1 )
-		return;
+	if ( $wp_query->max_num_pages <= 1 ) {
+		return; }
 
 	$paged = get_query_var( 'paged' ) ? absint( get_query_var( 'paged' ) ) : 1;
 	$max   = intval( $wp_query->max_num_pages );
 
 	/** Add current page to the array */
-	if ( $paged >= 1 )
-		$links[] = $paged;
+	if ( $paged >= 1 ) {
+		$links[] = $paged; }
 
 	/** Add the pages around the current page to the array */
 	if ( $paged >= 3 ) {
@@ -201,8 +201,8 @@ function child_numeric_posts_nav() {
 	echo '<div class="post-navigation"><ul>' . "\n";
 
 	/** Previous Post Link */
-	if ( get_previous_posts_link() )
-		printf( '<li>%s</li>' . "\n", get_previous_posts_link() );
+	if ( get_previous_posts_link() ) {
+		printf( '<li>%s</li>' . "\n", get_previous_posts_link() ); }
 
 	/** Link to first page, plus ellipses if necessary */
 	if ( ! in_array( 1, $links ) ) {
@@ -210,8 +210,8 @@ function child_numeric_posts_nav() {
 
 		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( 1 ) ), '1' );
 
-		if ( ! in_array( 2, $links ) )
-			echo '<li>…</li>';
+		if ( ! in_array( 2, $links ) ) {
+			echo '<li>…</li>'; }
 	}
 
 	/** Link to current page, plus 2 pages in either direction if necessary */
@@ -223,16 +223,16 @@ function child_numeric_posts_nav() {
 
 	/** Link to last page, plus ellipses if necessary */
 	if ( ! in_array( $max, $links ) ) {
-		if ( ! in_array( $max - 1, $links ) )
-			echo '<li>…</li>' . "\n";
+		if ( ! in_array( $max - 1, $links ) ) {
+			echo '<li>…</li>' . "\n"; }
 
 		$class = $paged == $max ? ' class="active"' : '';
 		printf( '<li%s><a href="%s">%s</a></li>' . "\n", $class, esc_url( get_pagenum_link( $max ) ), $max );
 	}
 
 	/** Next Post Link */
-	if ( get_next_posts_link() )
-		printf( '<li>%s</li>' . "\n", get_next_posts_link() );
+	if ( get_next_posts_link() ) {
+		printf( '<li>%s</li>' . "\n", get_next_posts_link() ); }
 
 	echo '</ul></div>' . "\n";
 
