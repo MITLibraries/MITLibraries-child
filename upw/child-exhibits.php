e6ba5aedb318950a1ca51ef10647170008f9e726
Matt Bernhardt <mjbernha@mit.edu>
2016-06-20 12:10:49 -0400

    Automated cleaning of Generic.ControlStructures.InlineControlStructure

--- upw/child-exhibits.php
+++ upw/child-exhibits.php
@@ -109,7 +109,7 @@ foreach ( (get_the_category()) as $category ) {
 						$last_value = end( $custom_field_values );
 						foreach ( $custom_field_values as $value ) {
 						  echo $value;
-						  if ($value != $last_value) echo ' , ';
+						  if ($value != $last_value) { echo ' , '; }
 						}
 					  }
 					  ?>
