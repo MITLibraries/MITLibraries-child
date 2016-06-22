a42a83c02e0084d7ba0a62654564fa101fca8357
Matt Bernhardt <mjbernha@mit.edu>
2016-06-22 09:20:28 -0400

    Escapes output

--- inc/location.php
+++ inc/location.php
@@ -12,16 +12,16 @@ $docs_email = get_post_meta( 1028, 'email', true );
 $docs_building = get_post_meta( 1028, 'building', true );
 $docs_slug = $docs->post_name;
 
-$map_page = '/locations/#!';
+$map_page = '/locations/#!' . $docs_slug;
 
 ?>
 
 <div class="info-more">
-	<a href="tel:<?php echo $docs_phone; ?>" class="phone"><?php echo $docs_phone; ?></a> |
-		<?php if ( $docs_email ) : ?>
-	<a href="mailto:<?php echo $docs_email; ?>" class="email"><?php echo $docs_email; ?></a> |
-		<?php endif; ?>
-	<a href="<?php echo $map_page.$docs_slug; ?>">Room: <?php echo $docs_building; ?></a>
+	<a href="tel:<?php echo esc_attr( $docs_phone ); ?>" class="phone"><?php echo esc_html( $docs_phone ); ?></a> |
+	<?php if ( $docs_email ) : ?>
+	<a href="mailto:<?php echo esc_attr( $docs_email ); ?>" class="email"><?php echo esc_html( $docs_email ); ?></a> |
+	<?php endif; ?>
+	<a href="<?php echo esc_attr( $map_page ); ?>">Room: <?php echo esc_html( $docs_building ); ?> <i class="icon-arrow-right"></i></a>
 </div>
 <div class="hours-today">
 	<span>Today's Hours: <span data-location-hours="Document Services"></span></span>  | 
