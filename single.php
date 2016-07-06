1a4abc032773c1ca45831bcbbd631272416b5e1e
Matt Bernhardt <mjbernha@mit.edu>
2016-07-06 11:29:47 -0400

    Cleans single.php

--- single.php
+++ single.php
@@ -3,43 +3,45 @@
  *
  * This is the template that displays single posts
  *
- * @package MIT_Libraries_Parent
- * @since 1.4.0
+ * @package MIT_Libraries_Child
+ * @since 2.1.0
  */
 
-get_header(); ?>
+get_header();
 
-<?php get_template_part('inc/breadcrumbs', 'child') ?>
-	
+get_template_part( 'inc/breadcrumbs', 'child' );
+
+?>
+
 <div id="stage" class="inner" role="main">
 
-	<?php get_template_part('inc/postHead'); ?>
-			
+	<?php get_template_part( 'inc/postHead' ); ?>
+
 		<div id="content" class="content has-sidebar">
 
-				<div class="content-main main-content">			
-					
+				<div class="content-main main-content">
+
 					<?php while ( have_posts() ) : the_post(); ?>
-					
+
 						<div class="article-head">
-							 
+
 						<span class="meta-header">News</span>
-						
+
 						<h2><?php the_title(); ?></h2>
-						
-						<p class="entry-meta"> posted <?php 
-							the_time('F j, Y');
-							
+
+						<p class="entry-meta"> posted <?php
+							the_time( 'F j, Y' );
+
 							$cats = array();
-							foreach ( get_the_category ( $post_id ) as $cat ) {
-							$categ = get_category( $cat ) ;
-							array_push($cats, $categ -> name) ;
+							foreach ( get_the_category( $post_id ) as $cat ) {
+								$categ = get_category( $cat );
+								array_push( $cats, $categ -> name );
 							}
 
-							$post_categories = implode ( ', ', $cats ) ;
+							$post_categories = implode( ', ', $cats );
 
-							echo ' in ' . $post_categories;
-							
+							echo ' in ' . esc_html( $post_categories );
+
 							?>
 						</p>
 						
@@ -51,7 +53,7 @@ get_header(); ?>
 								
 					
 									
-					<?php endwhile; // end of the loop. ?>
+					<?php endwhile; // End of the loop. ?>
 				</div>
 				
 				<?php get_sidebar(); ?>					
