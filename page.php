c4d39a6882c7f2f02129fd4350c581a3bc552b82
pbruk <phillip.bruk@gmail.com>
2016-07-07 07:02:29 -0400

    Splits postHead.php into separate concerns: header and navbar; removes site-specific references. Resolves #74.

--- page.php
+++ page.php
@@ -27,9 +27,24 @@ get_header(); ?>
 
 		<div id="stage" class="inner" role="main">
 
-			<?php get_template_part( 'inc/postHead' ); ?>
+		<?php get_template_part( 'inc/header-child' );
 
-			<?php while ( have_posts() ) : the_post(); ?>
+	    	// Checks to see if menu has not been assigned. -->
+			if ( !has_nav_menu( 'child-nav' ) ) {
+				wp_nav_menu( array(
+					'theme_location' => 'child-nav',
+					'fallback_cb' => false) );			
+				
+			echo '' ;
+				
+			} else {
+	    	
+	    	// Otherwise, if menu has been assigned, get navbar. -->
+			get_template_part( 'inc/nav', 'child' );
+					
+			} 
+				
+			while ( have_posts() ) : the_post(); ?>
 
 			<?php if ( is_active_sidebar( 'sidebar' ) ) { ?>
 
