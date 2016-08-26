18e4b979061defc43a7c46a599703d0fe5f8bdf1
Matt Bernhardt <mjbernha@mit.edu>
2016-08-26 10:57:02 -0400

    Add child stylesheet with dependency upon parent

--- functions.php
+++ functions.php
@@ -29,10 +29,15 @@ add_action( 'wp_enqueue_scripts', 'enqueue_my_scripts' );
 
 /**
  * Add theme-specific stylesheets
+ *
+ * @link http://mor10.com/challenges-new-method-inheriting-parent-styles-wordpress-child-themes/
  */
 function enqueue_my_styles() {
+	// First we enqueue style libraries.
 	wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css' );
+	// Then enqueues the child stylesheet with a dependency on the parent style, which _should_ enforce correct load order.
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'libraries-global' ) );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_my_styles' );
 
