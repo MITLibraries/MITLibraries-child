7dec42dfd28c61c4f59197911515ffc994053d4f
Frances Botsford <frances@switchingprotocols.com>
2016-09-27 11:56:17 -0400

    add gallery name to single exhibit view

--- single-exhibits.php
+++ single-exhibits.php
@@ -24,6 +24,9 @@ get_template_part( 'inc/breadcrumbs', 'child' );
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<div class="article-head">
+					<?php foreach ( (get_the_category()) as $category ) { ?>
+						<p class="title-sub"><?php echo esc_attr( $category->cat_name ); ?> Exhibit</p>
+					<?php } ?>
 
 					<h2><?php the_title(); ?></h2>
 					<?php if ( get_field( 'subtitle' ) ) : ?>
