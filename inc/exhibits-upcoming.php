ad3c8b9debd8654b1d8e8dc1c4a6f6ac66fadfe0
hllavina <hattie@libme25-0493314.mit.edu>
2017-05-09 14:49:22 -0400

    Exhibit updates to use excerpt field

--- inc/exhibits-upcoming.php
+++ inc/exhibits-upcoming.php
@@ -34,7 +34,7 @@ the_field( 'exhibit_thumbnail_image' ); ?>');">
 					<div class="category-post-content">
 						<h4><a class="exhibit-title" href="<?php the_permalink(); ?>"><?php the_title();?></a></h4>
 						<div class="entry-summary">
-							<p><?php custom_excerpt( 35, '...' ) ?></p>
+							<p><?php the_field( 'excerpt' ); ?></p>
 						</div>
 						<div class="exhibit-ends">
 							Opens <?php the_field( 'start_date' ); ?>
