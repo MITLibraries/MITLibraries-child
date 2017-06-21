2eb5d18e65596a86de145b9a0f4b3a8177e32dc3
hllavina <hattie@libme25-0493314.mit.edu>
2017-06-21 16:43:59 -0400

    Posts feed and content page, pagination
    Codesniifer issues
    
    Codesniffer issues
    
    Codesniffer issues
    
    Codesniffer issues
    
    Code climate issues
    
    Fix related posts and code climate
    
    Fixing inline structures
    
    Code climate

---
+++ page-posts-feed.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Template Name: Posts Feed Page
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package MIT_Libraries_Child
+ * @since Twenty Twelve 1.0
+ */
+
+$pageRoot = getRoot( $post );
+$section = get_post( $pageRoot );
+$isRoot = $section->ID === $post->ID;
+
+
+get_header();
+
+get_template_part( 'inc/breadcrumbs', 'child' ); ?>
+
+
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		
+
+		<div id="stage" class="inner" role="main">
+
+			<?php get_template_part( 'inc/postHead' ); ?>
+			
+			<?php if ( is_active_sidebar( 'sidebar' ) ) { ?>
+			
+				<div id="content" class="content has-sidebar">
+					<div class="main-content">
+						<div class="entry-content">
+							<?php
+							$title = get_the_title();
+							if ( '' !== $title ) :
+								if ( ! is_front_page() ) { echo '<h1>' . esc_html( $title ) . '</h1>'; }
+							endif;
+
+							the_content();
+							?>
+							
+							<?php get_template_part( 'inc/content', 'feed' ); ?>
+
+						</div>
+					</div>
+				<?php get_sidebar(); ?>
+
+			</div>
+			
+			<?php } else { ?> 
+			
+			<div id="content" class="content">
+
+				<?php get_template_part( 'inc/content', 'feed' ); ?>
+
+			</div>
+			
+			<?php } ?>
+		
+		</div><!-- end div#stage -->
+		
+	<?php endwhile; // End of the loop. ?>
+
+
+<?php get_footer(); ?>
