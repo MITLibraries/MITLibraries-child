33cfe1ca7c7b53827f8c93237af154efea114783
hllavina <hattie@libme25-0493314.mit.edu>
2017-06-22 16:17:34 -0400

    Fixing inline structures

--- single.php
+++ single.php
@@ -67,35 +67,35 @@ get_template_part( 'inc/breadcrumbs', 'child' );
 
 			if ( $tagcount > 0 ) {
 			 	$tag_ids = array();
-		  		foreach ( $tags as $individual_tag )
+		  		foreach ( $tags as $individual_tag ) {
 		  			$tag_ids[] = $individual_tag->term_id;
 					$args = array(
 					  	'tag__in' => $tag_ids,
 					  	'post__not_in' => array( $current_post_id ),
 					  	'posts_per_page' => 3,
 					  	);
+					}
+			  	$related_query = new WP_Query( $args );
 
-			  			$related_query = new WP_Query( $args ); 
+				if ( $related_query->have_posts() ) :
+				?>
+					<hr/>
+					<div>
+					<h3>Related posts</h3>
+						<ul>
+ 						<?php
+ 						while ( $related_query->have_posts() ) : $related_query->the_post(); ?>
 
-						if ( $related_query->have_posts() ) :
-						?>
-							<hr/>
-							<div>
-							<h3>Related posts</h3>
-								<ul>
-		 						<?php
-		 						while ( $related_query->have_posts() ) : $related_query->the_post(); ?>
-
-									<div class="relatedthumb">
-									    <li><a rel="external" href="<?php the_permalink()?>">
-									    <?php the_post_thumbnail( array( 100, 100 ) ); ?> &nbsp;
-									    <?php the_title(); ?>
-									    </a></li>
-									</div>
-								<?php endwhile; ?>
-		    					</ul>
-		    				</div>
-	      				<?php endif;?>
+							<div class="relatedthumb">
+							    <li><a rel="external" href="<?php the_permalink()?>">
+							    <?php the_post_thumbnail( array( 100, 100 ) ); ?> &nbsp;
+							    <?php the_title(); ?>
+							    </a></li>
+							</div>
+						<?php endwhile; ?>
+    					</ul>
+    				</div>
+  				<?php endif;?>
 		   
 			<?php
 
