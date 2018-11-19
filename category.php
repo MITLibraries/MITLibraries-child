ca1eae1d08cb37e86683c68b8fa06bb98b22d045
Frances Botsford <frances@switchingprotocols.com>
2018-11-19 11:00:37 -0500

    remove upw code from category page, revert to twentytwelve

--- category.php
+++ category.php
@@ -15,12 +15,10 @@ $section = get_post( $pageRoot );
 $isRoot = $section->ID == $post->ID;
 
 
-get_header( 'child' );
+get_header( 'child' ); ?>
 
-get_template_part( 'inc/breadcrumbs', 'category' ); ?>
+<?php get_template_part( 'inc/breadcrumbs', 'category' ); ?>
 
-
-	
 <div id="stage" class="inner" role="main">
 
 	<?php get_template_part( 'inc/postHead' ); ?>
@@ -28,96 +26,41 @@ get_template_part( 'inc/breadcrumbs', 'category' ); ?>
 	<div id="content" class="content has-sidebar">
 		<div class="content-main main-content">
 				
-				<?php if ( is_category() ) {
-					echo '<h1>';
-					single_cat_title();
-					echo '</h1>';
-					}
-				?>
-				<div id="sticky-posts-2" class="widget-1 widget-first widget-last widget-odd hide-title widget widget_ultimate_posts" role="complementary">
+		<?php if ( have_posts() ) : ?>
+			<header class="archive-header">
+				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
 
-					<?php
-					if ( have_posts() ) : ?>
-						<div class="upw-posts hfeed">
-						<?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
-							<?php $current_post = ( $post->ID === $current_post_id && is_single() ) ? 'active' : '';
-							$custom_fields = get_fields( $current_post_id ); ?>
+			<?php if ( category_description() ) : // Show an optional category description ?>
+				<div class="archive-meta"><?php echo category_description(); ?></div>
+			<?php endif; ?>
+			</header><!-- .archive-header -->
 
-							<article <?php post_class( $current_post ); ?>>
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
 
-							  	<header>
-								  	<h4 class="entry-title">
-										<a href="<?php the_permalink(); ?>" rel="bookmark">
-										 	<?php the_title(); ?>
-										</a>
-									 </h4>
-								</header>
-								 <div class="upw-content">
-									  
-									<?php if ( $custom_fields ) : ?>
-											<?php $custom_field_name = explode( ',', $custom_fields ); ?>
-										<div class="entry-custom-fields">
-											<?php foreach ( $custom_field_name as $name ) :
-											  	$name = trim( $name );
-											  	$custom_field_values = get_post_meta( $post->ID, $name, true );
-											  	if ( $custom_field_values ) : ?>
-													<span class="custom-field custom-field-<?php echo esc_html( $name ); ?>">
-													<?php
-													if ( ! is_array( $custom_field_values ) ) {
+				/* Include the post format-specific template for the content. If you want to
+				 * this in a child theme then include a file called content-___.php
+				 * (where ___ is the post format) and that will be used instead.
+				 */
+				get_template_part( 'content', get_post_format() );
 
-														// For custom fields named "event_date", we pass the value through an additional parsing step.
-														if ( 'event_date' === $name ) {
-														  	$event_date = date_parse_from_format( 'Ymd', $custom_field_values );
-														  	$custom_field_values = date( 'F j, Y', mktime( 0, 0, 0, $event_date['month'],$event_date['day'],$event_date['year'] ) );
-														}
+			endwhile;
 
-														echo esc_html( $custom_field_values );
+			twentytwelve_content_nav( 'nav-below' );
+			?>
 
-													} else {
-														$last_value = end( $custom_field_values );
-														foreach ( $custom_field_values as $value ) {
-														  	echo esc_html( $value );
-														  	if ( $value !== $last_value ) { echo ', '; }
-														}
-													}
-													?>
-													</span>
-												<?php endif;
-											endforeach; ?>
-										 </div>
-									<?php endif; ?>
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		<?php endif; ?>
 
-									<div class="entry-summary">
-										<?php if ( get_first_post_image() ) : ?>
-										<img src="<?php echo esc_attr( get_first_post_image() ); ?>" width="200" >
-										<?php endif; ?>
-									  <p>
-										<?php echo esc_html( custom_excerpt( 100,'...' ) ); ?>
-									  </p>
-									</div>
-									 <footer>
-										<div class="entry-meta">
-											<time class="published" datetime="<?php echo esc_attr( get_the_time( 'c' ) ); ?>"><?php echo esc_html( get_the_time( 'F j, Y' ) ); ?></time>
-													
-								  		</div>
-									</footer>
-					  			</div>
-							</article>
-				  		<?php endwhile; ?>
-					<?php endif; ?>
-			
-					<ul class="post-navigation">
-						<li><?php previous_posts_link( 'Newer Posts' ) ?></li>
-						<li><?php next_posts_link( 'Older Posts', $custom_query->max_num_pages ) ?></li>
-					</ul>
+		</div><!-- .mainContent -->
 
-					</div>
-				</div>
-			</div>
-		</div>
-	<?php get_sidebar(); ?>	
-	</div>
-		
-</div><!-- end div#stage -->
+		<?php get_sidebar(); ?>
+
+	</div><!-- .allContent -->
+
+</div><!-- #stage -->
 		
 <?php get_footer(); ?>
