349355a82ca27f9eb3d7f7a0a8522c23f0b595d6
Frances Botsford <frances@switchingprotocols.com>
2018-11-20 16:49:38 -0500

    test fixes

--- inc/nav-child.php
+++ inc/nav-child.php
@@ -14,21 +14,21 @@
 
 	
 	<nav class="navbar navbar-default" role="navigation">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-	    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-	  <a class="navbar-brand mobile-display">
-                <?php _e( 'View Menu' ); ?>
-            </a>
-    </div>
+	<div class="container-fluid">
+		<!-- Brand and toggle get grouped for better mobile display -->
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand mobile-display">
+				<?php esc_html_e( 'View Menu' ); ?>
+			</a>
+		</div>
 
-        <?php
+				<?php
 			wp_nav_menu(
 				array(
 					'menu'              => 'Sub Nav',
@@ -47,13 +47,13 @@
 		?>
 
 
-    </div>
+		</div>
 </nav>
 <script>
 	$('.dropdown-toggle').click(function() {
-    var location = $(this).attr('href');
-    window.location.href = location;
-    return false;
+		var location = $(this).attr('href');
+		window.location.href = location;
+		return false;
 });
 </script>
 
--- inc/post-trimmed.php
+++ inc/post-trimmed.php
@@ -6,24 +6,12 @@
  * @since 2.0.0
  */
 
-	$postID = get_the_ID();
-
-	if ( is_home() ) {
-		$postHeader = '<h2 class="article-title" id="post-'.$postID.'">';
-		$closeTag = '</h2>';
-	}
-
-	if ( is_search() || is_category() ) {
-		$postHeader = '<h3 class="article-title" id="post-'.$postID.'">';
-		$closeTag = '</h3>';
-	}
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="article-head">
-		<?php echo $postHeader; ?><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a><?php echo $closeTag; ?>
-		<p class="entry-meta">Posted <?php the_time( 'F jS, Y' ) ?> by <?php the_author() ?></p>
+		<h2 class="article-title" id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+		<p class="entry-meta">Posted <?php the_time( 'F jS, Y' ); ?> by <?php the_author(); ?></p>
 	</header>
 	
 	<div class="entry-content">
@@ -31,7 +19,7 @@
 	</div>
 
 	<footer class="entry-meta">
-		<p class="postmetadata entry-meta">Posted in <?php the_category( ', ' ) ?> 
+		<p class="postmetadata entry-meta">Posted in <?php the_category( ', ' ); ?> 
 		<?php edit_post_link( '(Edit)' ); ?> </p>
 	</footer>
 </article>
