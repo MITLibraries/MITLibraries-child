cf1de4aa1c02710e087a19f53768364acc1cff21
Frances Botsford <frances@switchingprotocols.com>
2018-11-20 15:04:13 -0500

    adjust front page

--- inc/post-trimmed.php
+++ inc/post-trimmed.php
@@ -9,28 +9,29 @@
 	$postID = get_the_ID();
 
 	if ( is_home() ) {
-		$postHeader = '<h2 id="post-'.$postID.'">';
+		$postHeader = '<h2 class="article-title" id="post-'.$postID.'">';
 		$closeTag = '</h2>';
 	}
 
 	if ( is_search() || is_category() ) {
-		$postHeader = '<h3 id="post-'.$postID.'">';
+		$postHeader = '<h3 class="article-title" id="post-'.$postID.'">';
 		$closeTag = '</h3>';
 	}
 
 ?>
 
-<div class="post">
-
-	<?php echo $postHeader; ?><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a><?php echo $closeTag; ?>
-	<small>Posted <?php the_time( 'F jS, Y' ) ?> by <?php the_author() ?> </small>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="article-head">
+		<?php echo $postHeader; ?><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a><?php echo $closeTag; ?>
+		<p class="entry-meta">Posted <?php the_time( 'F jS, Y' ) ?> by <?php the_author() ?></p>
+	</header>
 	
-	<div class="entry">
+	<div class="entry-content">
 		<?php the_content( 'Read the rest of this entry &raquo;' ); ?>
 	</div>
 
-	<p class="postmetadata">Posted in <?php the_category( ', ' ) ?> 
-
-	 <?php edit_post_link( '(Edit)' ); ?> </p>
-
-</div>
+	<footer class="entry-meta">
+		<p class="postmetadata entry-meta">Posted in <?php the_category( ', ' ) ?> 
+		<?php edit_post_link( '(Edit)' ); ?> </p>
+	</footer>
+</article>
