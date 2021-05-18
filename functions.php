f06d083b09f95f0315a24ff17de2884897b1ae2b
Matt Bernhardt <mjbernha@mit.edu>
2021-05-18 15:18:23 -0400

    Rework styles around staff widgets
    ** Why are these changes being introduced:
    
    * The staff widgets feature (used on the AKDC site) is not displaying
      the four current widgets with the right spacing. Details in the
      ticket.
    * Additionally, there is a (very justified) request to allow widget
      spacing to be controlled using a number of pre-defined classes, which
      are actually already in the theme.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/UXWS-1130
    
    ** How does this address that need:
    
    * This strips out some hard-coded styles for the staff-widget class,
      which locked the widget to a certain display width. Instead, site
      builders should use the existing `twoup-widget` and `threeup-widget`
      classes to define whether these widgets should appear in 2x widths or
      3x widths.
    * Some spacing around the widget display is also adjusted, to make sure
      that responsive behavior  - and adequate spacing - are preserved.
    
    ** Document any side effects to this change:
    
    * This bumps the theme version to 2.2.4, which includes the cache-
      busting version string used to load the stylesheet.

--- functions.php
+++ functions.php
@@ -37,7 +37,7 @@ function enqueue_my_styles() {
 	wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
 	wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css' );
 	// Then enqueues the child stylesheet with a dependency on the parent style, which _should_ enforce correct load order.
-	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'libraries-global' ), '2.2.3' );
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'libraries-global' ), '2.2.4' );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_my_styles' );
 
