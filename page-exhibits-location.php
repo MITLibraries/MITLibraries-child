f37e2ee412a37a18aa9c2ca8d0315d11b866bc1d
Matt Bernhardt <mjbernha@mit.edu>
2022-08-18 12:29:03 -0400

    Adds abstracted "exhibits location" template
    ** Why are these changes being introduced:
    
    * Rather than having bespoke page templates with hard-coded values for
      specific locations, we need a single template which is capable of
      showing exhibit listings or any location.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/uxws-1396
    
    ** How does this address that need:
    
    * This creates a new page template, page-exhibits-location.php, which
      is capable of displaying exhibit information for any single location.
      It reads the first category assigned to a Page record, loads the
      Exhibit records which have been also placed in that category, and
      displays them to the user.
    
    ** Document any side effects to this change:
    
    * While a Page using this template _can_ be placed in multiple
      categories, only the first term returned by get_the_category() will
      be used by the page. I am still working out how to present this
      aspect to site builders...
    
    Fixup page-exhibits-location.php

---
+++ page-exhibits-location.php
@@ -0,0 +1,209 @@
+<?php
+/**
+ * Template Name: Exhibits | Location Index
+ *
+ * @package MIT_Libraries_Child
+ * @since Twenty Twelve 1.0
+ *
+ * This template displays relevant exhibits for a given location, specified
+ * in page content by assigning the page to a given category.
+ *
+ * To use this template:
+ * 1. Create a Page record. The title and body are irrelevant and are not
+ *    displayed by this template.
+ * 2. Assign the Page a single Category value for the location you wish to
+ *    display (For example, "Rotch Library"). PLEASE NOTE: assigning multiple
+ *    Category values to the Page should be avoided - only the first value
+ *    returned by get_the_category() will be used.
+ * 3. Set the Page to display using this page template.
+ * 4. The page will now show Exhibit records which share the same category.
+ */
+
+get_header( 'child' );
+
+// Pages using this template should have one and only one category value. If
+// the page is placed in multiple categories, then the first term returned here
+// will determine which sets of exhibits get displayed.
+$categories    = get_the_category();
+$location_name = $categories[0]->name;
+?>
+
+	<?php get_template_part( 'inc/breadcrumbs', 'child' ); ?>
+
+	<div id="stage" class="inner" role="main">
+
+	<?php get_template_part( 'inc/postHead' ); ?>
+
+		<div id="content" class="content has-sidebar">
+
+			<div class="main-content">
+
+			<?php
+
+			$current_query = new WP_Query(
+				array(
+					'post_type'           => 'exhibits',  // Only query exhibits.
+					'posts_per_page'      => 10,
+					'ignore_sticky_posts' => false,
+					'meta_key'            => 'start_date',
+					'orderby'             => 'start_date',
+					'order'               => 'DESC',      // Descending, so later events first.
+					'meta_query'          => array(
+						array(
+							'key'     => 'start_date',     // Which meta to query.
+							'value'   => gmdate( 'Y-m-d' ),  // Value for comparison.
+							'compare' => '<=',             // Method of comparison.
+							'type'    => 'DATE',
+						),
+						array(
+							'key'     => 'end_date',       // Which meta to query.
+							'value'   => gmdate( 'Y-m-d' ),  // Value for comparison.
+							'compare' => '>=',             // Method of comparison.
+							'type'    => 'DATE',
+						),
+						array(
+							'key'     => 'location',       // Which meta to query.
+							'value'   => $location_name,  // Value for comparison.
+							'compare' => '=',             // Method of comparison.
+							'type'    => 'CHAR',
+						), // The meta_query is an array of query items.
+					), // End meta_query array.
+				) // End array.
+			); // Close WP_Query constructor call.
+			?>
+
+			<div class="exhibits-feed-section">
+
+				<h3 class="title-sub">Current Exhibits</h3>
+
+				<?php
+				if ( $current_query->have_posts() ) :
+					while ( $current_query->have_posts() ) :
+						$current_query->the_post(); // Loop for current exhibits.
+
+						get_template_part( 'inc/exhibits-detail' );
+
+					endwhile;
+
+						wp_reset_postdata();
+
+				else :
+					?>
+
+					<p><?php esc_html_e( 'There are no current exhibit announcements at this time. New exhibits are added throughout the year, so please check back.' ); ?></p>
+
+				<?php endif; ?>
+
+			</div>
+
+		<!-- END OF CURRENT EXHIBITS LOOP -->
+
+		<?php
+
+		$future_query = new WP_Query(
+			array(
+				'post_type'      => 'exhibits',  // Only query exhibits.
+				'meta_key'       => 'start_date',
+				'posts_per_page' => 10,
+				'orderby'        => 'start_date',
+				'order'          => 'ASC',        // Descending, so later events first.
+				'meta_query'     => array(
+					array(
+						'key'     => 'start_date',     // Which meta to query.
+						'value'   => gmdate( 'Y-m-d' ),  // Value for comparison.
+						'compare' => '>',             // Method of comparison.
+						'type'    => 'DATE',
+					),
+					array(
+						'key'     => 'location',       // Which meta to query.
+						'value'   => $location_name,  // Value for comparison.
+						'compare' => '=',             // Method of comparison.
+						'type'    => 'CHAR',
+					), // The meta_query is an array of query items.
+				), // End meta_query array.
+			) // End array.
+		); // Close WP_Query constructor call.
+		?>
+
+			<div class="exhibits-feed-section">
+
+				<h3 class="title-sub">Upcoming Exhibits</h3>
+
+				<?php
+				if ( $future_query->have_posts() ) :
+					while ( $future_query->have_posts() ) :
+						$future_query->the_post(); // Loop for future exhibits.
+
+						get_template_part( 'inc/exhibits-detail' );
+
+					endwhile;
+
+					wp_reset_postdata();
+
+				else :
+					?>
+
+					<p><?php esc_html_e( 'There are no upcoming exhibit announcements at this time. New exhibits are added throughout the year, so please check back.' ); ?></p>
+
+				<?php endif; ?>
+
+			</div>
+
+		<!-- END OF UPCOMING EXHIBITS LOOP -->
+
+		<?php
+
+		$past_query = new WP_Query(
+			array(
+				'post_type'      => 'exhibits',  // Only query exhibits.
+				'meta_key'       => 'end_date',
+				'posts_per_page' => 5,
+				'orderby'        => 'end_date',
+				'order'          => 'DESC',      // Descending, so later events first.
+				'meta_query'     => array(
+					array(
+						'key'     => 'end_date',       // Which meta to query.
+						'value'   => gmdate( 'Y-m-d' ),  // Value for comparison.
+						'compare' => '<',              // Method of comparison.
+						'type'    => 'DATE',
+					),
+					array(
+						'key'     => 'location',       // Which meta to query.
+						'value'   => $location_name,  // Value for comparison.
+						'compare' => '=',             // Method of comparison.
+						'type'    => 'CHAR',
+					), // The meta_query is an array of query items.
+				), // End meta_query array.
+			) // End array.
+		); // Close WP_Query constructor call.
+
+		?>
+
+			<div class="exhibits-feed-section">
+
+				<h3 class="title-sub">Past Exhibits</h3>
+
+			<?php
+			while ( $past_query->have_posts() ) :
+				$past_query->the_post(); // Loop for events.
+
+				get_template_part( 'inc/exhibits-detail' );
+
+				wp_reset_postdata(); // Restore global post data stomped by the_post().
+
+			endwhile; // End of the loop.
+			?>
+
+			</div>
+			<!-- END OF UPCOMING EXHIBITS LOOP -->
+			<a class="button-secondary exhibits-button" href="/exhibits/past-exhibits/">View all past exhibits</a>
+
+		</div>  <!-- main-content --> 
+
+		<?php get_sidebar(); ?>
+
+		</div>   <!-- content --> 
+
+	</div>   <!-- stage --> 
+
+<?php get_footer(); ?>
